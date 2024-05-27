f6a518e8699fd51119b6b0e6375f79e78a78834a
linzrabo <36148964+linzrabo@users.noreply.github.com>
2024-05-27 11:27:25 -0700

    Create index.web.js

---
+++ index.web.js
@@ -0,0 +1,9 @@
+import '#/platform/markBundleStartTime'
+
+import '#/platform/polyfills'
+import {registerRootComponent} from 'expo'
+import {doPolyfill} from '#/lib/api/api-polyfill'
+import App from '#/App'
+
+doPolyfill()
+registerRootComponent(App)
